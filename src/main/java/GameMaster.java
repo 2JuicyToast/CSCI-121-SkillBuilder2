05f8e03aa430eb87865f035b8856073743f5ff5d
Bacon <parkerjoshua0403@gmail.com>
2026-02-12 12:44:06 -0500

    SkillBuilder Complete

--- src/main/java/GameMaster.java
+++ src/main/java/GameMaster.java
@@ -1,3 +1,20 @@
 public class GameMaster {
+    private static int totalExperiencePoints;
 
+    public static void gainExperience(int xp){
+        totalExperiencePoints += xp;
+    }
+
+    public static int getTeamXP(){
+        return totalExperiencePoints;
+    }
+
+    public static void applyPowerUp(int xp){
+        totalExperiencePoints += xp;
+    }
+
+    public static void applyPowerUp(double multiplier){
+        int xPBoost = (int)(totalExperiencePoints * multiplier);
+        totalExperiencePoints = xPBoost;
+    }
 }
