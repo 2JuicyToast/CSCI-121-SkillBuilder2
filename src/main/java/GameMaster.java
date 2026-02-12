708c9fef6ad67ce692ce838f4799ad8b88fc54fa
Bacon <parkerjoshua0403@gmail.com>
2026-02-12 12:50:40 -0500

    SkillBuilder Complete

--- src/main/java/GameMaster.java
+++ src/main/java/GameMaster.java
@@ -16,5 +16,19 @@ public class GameMaster {
     public static void applyPowerUp(double multiplier){
         int xPBoost = (int)(totalExperiencePoints * multiplier);
         totalExperiencePoints = xPBoost;
+        }
+
+        public static void main(String[] args) {
+        // 1) Add 500 XP via quest
+        GameMaster.gainExperience(500);
+
+        // 2) Apply a Potion of 100 XP
+        GameMaster.applyPowerUp(100);
+
+        // 3) Apply an Artifact multiplier of 2.5
+        GameMaster.applyPowerUp(2.5);
+
+        // 4) Print final XP
+        System.out.println(GameMaster.getTeamXP());
     }
 }
